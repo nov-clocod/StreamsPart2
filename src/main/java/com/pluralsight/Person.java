a4eada8909bd60b39faf0a6e09d998eebd2de766
Sthealth <AW@example.com>
2025-11-05 10:22:31 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,30 @@
+package com.pluralsight.streams;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String lastName, String firstName, int age) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + '|' + lastName + '|' + age;
+    }
+}
